05407ee66e4120e1a84b733b72ea79a8cff82cf0
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-09 17:38:44 -0300

    FEAT(CalculadoraServiceTestes): Criando classe CalculadoraServiceTeste para teste

---
+++ src/test/java/br/com/alura/tdd/service/CalculadoraServiceTestes.java
@@ -0,0 +1,19 @@
+package br.com.alura.tdd.service;
+
+public class CalculadoraServiceTestes {
+    public static void main(String[] args) {
+        CalculadoraService calc = new CalculadoraService();
+
+        int soma = calc.somar(3,7);
+        System.out.println(soma);
+
+        soma = calc.somar(3,0);
+        System.out.println(soma);
+
+        soma = calc.somar(0,0);
+        System.out.println(soma);
+
+        soma = calc.somar(3,-1);
+        System.out.println(soma);
+    }
+}
